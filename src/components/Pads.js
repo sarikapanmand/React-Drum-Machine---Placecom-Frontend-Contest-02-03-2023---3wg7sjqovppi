76b171b9133cf85e0966cc2e6577b064f8a4228e
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2023-03-02 19:10:01 +0530

    Update Pads.js

--- src/components/Pads.js
+++ src/components/Pads.js
@@ -3,24 +3,27 @@ import Pad from "./Pad";
 import {bank1} from "./App"
 
 
-function Pads() {
+function Pads({power}) {
     const keypadCode = Object.keys(bank1);
+    const[audioName,setAudioName]=useState(null);
 
 
     const playSound = e => {
+        setAudioNAme(e.Name);
     }
 
 
     return (
       <div id='div-pads'>
         {keypadCode.map((pad, idx) => {
-          console.log(pad + idx)
+          //console.log(pad + idx)
           return (
             <Pad
               id={pad+idx}
               key={pad+idx}
               handleClick={playSound}
               element={pad} />
+                  power={power}
           );
         })}
         <div id='display'> Show the name of current audio here </div>  
